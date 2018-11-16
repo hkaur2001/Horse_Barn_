9b79c402847e4e0caeb2744a583964718d014a88
hkaur2001 <36794921+hkaur2001@users.noreply.github.com>
2018-11-16 15:07:11 -0500

    .

--- src/findHorseSpace.java
+++
@@ -1,7 +0,0 @@
-public int findHorseSpace(String name) {
-        for (int i = 0; i < this.spaces.length; i++) {
-            if (this.spaces[i]!=null && name.equals(this.spaces[i].getName())) {
-                return i;
-            }}return -1;
-}
-
