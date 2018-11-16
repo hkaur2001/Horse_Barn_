463e599360a153620fabb53da30ace950222d0c3
hkaur2001 <36794921+hkaur2001@users.noreply.github.com>
2018-11-16 15:05:15 -0500

    commit

--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -1,29 +1,26 @@
 public class HorseBarn {
     private Horse[] spaces;
 
-    public HorseBarn(horses) {
+    public HorseBarn(Horse[] horses) {
+        this.spaces = horses;
     }
 
-    private spaces;
     public int findHorseSpace(String name) {
-        for (int i = 0; i < this.spaces.length; i++) {
-            if (this.spaces[i]!=null && name.equals(this.spaces[i].getName())) {
+        for(int i = 0; i < this.spaces.length; i++) {
+            if (this.spaces[i] != null && name.equals(this.spaces[i].getName())) {
                 return i;
             }
         }
         return -1;
     }
     public void consolidate() {
-        for (int i = 0; i < this.spaces.length-1; i++) {
-            if (this.spaces[i] == null) {
-                for (int j = i+1; j < this.spaces.length; j++) {
-                    if (this.spaces[j] != null) {
-                        this.spaces[i] = this.spaces[j];
-                        this.spaces[j] = null;
-                        j = this.spaces.length;
-                    }
-                }
+        Horse[] newSpaces = new Horse[this.spaces.length];
+        int nextSpot = 0;
+        for (Horse nextHorse : this.spaces) {
+            if (nextHorse != null){
+                newSpaces[nextSpot] = nextHorse;
+                nextSpot++;
             }
         }
+        this.spaces = newSpaces;
     }
-}
